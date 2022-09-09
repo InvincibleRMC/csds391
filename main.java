bb27acb8ea6b02a12ce0d4f67c1182c94da4da99
Michael Carlstrom <rmc@carlstrom.com>
2022-09-09 11:05:44 -0400

    first day

---
+++ main.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
+
+public class main {
+
+    static Puzzle puzzle;
+    public static void main(String[] args) throws FileNotFoundException {
+
+        
+
+        // maybe try-catch could be more elegant
+        String fileName = "input.txt";
+        File inputFile = new File(fileName);
+        FileInputStream inputStream = new FileInputStream(inputFile);
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader input = new BufferedReader(inputStreamReader);
+        input.lines().forEach(command -> commandHandler(command));
+      
+
+        try {
+            input.close();
+        } catch (IOException e) {}
+    }
+
+    public static void commandHandler(String commandAndInput){
+
+        String[] commandInput = commandAndInput.split(" ");
+        
+        switch(commandInput[0]){
+            case "setState":{
+                puzzle.setState(commandInput[1]);
+            }
+            case "printState":{
+                puzzle.printState();
+            }
+            case "move":{
+                puzzle.move(commandInput[1]);
+            }
+            case "randomizeState":{
+                puzzle.randomizeState(commandInput[1]);
+            }
+            case "solve":{
+                switch(commandInput[1]){
+                    case "A-Star"
+                    puzzle.
+                }
+            }
+        }
+
+
+        
+    }
+
+}
+
+
